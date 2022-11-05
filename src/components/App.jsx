9fb9e88992922a18a85598e07ea31a0b098676f3
Serj96 <99637869+Serj96@users.noreply.github.com>
2022-11-05 10:51:52 +0200

    API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
-import './App.module.css'
+import './App.module.css';
 import CurrynceInput from './CurrynceInput/CurrynceInput';
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export function App() {
   const [amount1, setAmount1] = useState(1);
@@ -9,20 +10,32 @@ export function App() {
   const [currency1, currencyState1] = useState('USD');
   const [currency2, currencyState2] = useState('USD');
 
-  useEffect((() => {}));
+  const [rates, setRates] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.apilayer.com/fixer/latest?base=USD&apikey=cbpH1YcXGt3oyUUWvv3XLQmexNHIxCsv'
+      )
+      .then(response => {
+        setRates(response.data.rates);
+      });
+  });
 
   return (
     <div>
       <CurrynceInput
-        currencies={['USD']}
+        currencies={Object.keys(rates)}
         amount={amount1}
         currency={currency1}
       />
       <CurrynceInput
-        currencies={['USD']}
+        currencies={Object.keys(rates)}
         amount={amount2}
         currency={currency2}
       />
     </div>
   );
 }
+
+
