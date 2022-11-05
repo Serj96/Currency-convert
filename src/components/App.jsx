688131c18cd1ce5c91592a5cf4adbe0197a33359
Serj96 <99637869+Serj96@users.noreply.github.com>
2022-11-05 10:41:10 +0200

    add Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
+import './App.module.css'
+import CurrynceInput from './CurrynceInput/CurrynceInput';
+import {useState, useEffect} from 'react'
+
+export function App() {
+  const [amount1, setAmount1] = useState(1);
+  const [amount2, setAmount2] = useState(1);
+
+  const [currency1, currencyState1] = useState('USD');
+  const [currency2, currencyState2] = useState('USD');
+
+  useEffect((() => {}));
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <CurrynceInput
+        currencies={['USD']}
+        amount={amount1}
+        currency={currency1}
+      />
+      <CurrynceInput
+        currencies={['USD']}
+        amount={amount2}
+        currency={currency2}
+      />
     </div>
   );
-};
+}
